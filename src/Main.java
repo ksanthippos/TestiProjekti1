59273ec3e1dff859254c103a6a3bd83656070adc
ksanthippos <mikael.rauhala@gmail.com>
2020-07-24 08:18:11 +0000

    muutettu gitpodista 2

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,12 @@ public class Main {
     public static void main(String[] args) {
 
         int x = 5;
-        int y = 7;
+        int y = 4;
 
         System.out.println(x);
         System.out.println(y);
 
-        System.out.println("jees");
+        System.out.println("je");
 
     }
 }
